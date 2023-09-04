f6332bfe99a389ff087cb41c9916e77f90258a8a
Gerardo Solís Zarate <gsolis@grupopm.mx>
2023-09-04 11:53:33 -0600

    Ignorar paginación xls

--- src/test/java/com/imss/sivimss/usuarios/util/JsonUtil.java
+++ src/test/java/com/imss/sivimss/usuarios/util/JsonUtil.java
@@ -4,8 +4,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.util.ResourceUtils;
 
-import com.imss.sivimss.usuarios.util.JsonUtil;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
